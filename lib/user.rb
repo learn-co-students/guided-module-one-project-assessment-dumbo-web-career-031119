ecb4cfebe6b20c5cdd12407d7b90e30bddb07cdb
siberry <siberry226@gmail.com>
2019-03-27 15:22:43 -0400

    Revert "fixing"
    This reverts commit a4fd7c28caf09dcdfffb27aac0d0927a89999c6e.

--- lib/user.rb
+++ lib/user.rb
@@ -15,7 +15,6 @@ class User < ActiveRecord::Base
     end
   end
 
-<<<<<<< HEAD
   def total
     self.cart_items.map { |cartItem| cartItem.price}.sum
     # if totalz.to_s.split(".")[1].length == 1
@@ -54,38 +53,6 @@ class User < ActiveRecord::Base
    puts "\n"
  end
 
-=======
-  def cart_display
-    # if total.to_s.split(".")[1].length == 1
-    #   total = total.to_s + "0"
-    # else
-    #   total = total.round(2).to_s
-    # end
-  
-    puts "Your cart:"
-    cart.each do |item|
-      puts "1/8 oz. #{item.dispensary_inventory.strain.name} @ #{item.dispensary_inventory.dispensary.name}, $#{item.format_price}".magenta
-    end
-    puts ""
-    print " Your total is ".colorize(color: :blue, background: :cyan)
-    print "$"
-    puts self.total #.colorize(color: :red, background: :cyan)
-    puts "\n"
-  end
-
-  def buy_item_from_dispensary(dispensary_item)
-    CartItem.create(self.id,dispensary_item.id)
-  end
-
-  def total
-    totalz = self.cart_items.map { |cartItem| cartItem.price}.sum
-    if totalz.to_s.split(".")[1].length == 1
-      totalz = totalz.to_s + "0"
-    else
-      totalz = totalz.round(2).to_s
-    end
-  end
->>>>>>> parent of 0e23ae2... sharing
 
   def empty_cart
     self.cart_items.each { |cartItem| cartItem.delete}
